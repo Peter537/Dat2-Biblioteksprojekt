d3d2f1dd5a222b688b6c9df5ff118a03a713f523
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-02-20 12:01:32 +0100

    Create BrugerMapper.java

---
+++ src/mapper/BrugerMapper.java
@@ -0,0 +1,34 @@
+package mapper;
+
+import database.ConnectionConfiguration;
+import entities.Bruger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrugerMapper {
+
+    public static List<Bruger> getBrugerList() {
+        List<Bruger> brugerList = new ArrayList<>();
+        try {
+            Connection connection = ConnectionConfiguration.getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM bibliotek.bruger");
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                int id = result.getInt("idbruger");
+                String navn = result.getString("navn");
+                String adresse = result.getString("adresse");
+                int postnr = result.getInt("postnr");
+                brugerList.add(new Bruger(id, navn, adresse, postnr));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return brugerList;
+    }
+
+}
