6a33006afc678754e8455f0aeb9703ce31131244
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-02-20 12:09:52 +0100

    Entities

---
+++ src/entities/Udlaan.java
@@ -0,0 +1,38 @@
+package entities;
+
+public class Udlaan {
+
+    private int id;
+    private Bruger bruger;
+    private Bog bog;
+
+    public Udlaan(int id, Bruger bruger, Bog bog) {
+        this.id = id;
+        this.bruger = bruger;
+        this.bog = bog;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Bruger getBruger() {
+        return bruger;
+    }
+
+    public void setBruger(Bruger bruger) {
+        this.bruger = bruger;
+    }
+
+    public Bog getBog() {
+        return bog;
+    }
+
+    public void setBog(Bog bog) {
+        this.bog = bog;
+    }
+}
