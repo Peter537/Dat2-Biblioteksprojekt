736ba9693be39e723d6ed827411ddcdcddb9d40f
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-02-20 14:05:21 +0100

    Create UdlaanMapper.java

---
+++ src/mapper/UdlaanMapper.java
@@ -0,0 +1,12 @@
+package mapper;
+
+import entities.Udlaan;
+
+import java.util.List;
+
+public class UdlaanMapper {
+
+    public static List<Udlaan> getUdlaanList() {
+        return null;
+    }
+}
