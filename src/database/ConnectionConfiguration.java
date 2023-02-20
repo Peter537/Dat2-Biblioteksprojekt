15fa81f1225150dc36fa905d828e58b0bff7646e
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-02-20 11:12:37 +0100

    Create ConnectionConfiguration.java

---
+++ src/database/ConnectionConfiguration.java
@@ -0,0 +1,21 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionConfiguration {
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        String url = "jdbc:mysql://localhost:3306/bibliotekonsdag?serverTimezone=CET&useSSL=false";
+        String user = "root";
+        String password = "123";
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
