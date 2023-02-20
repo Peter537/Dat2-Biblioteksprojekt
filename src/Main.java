7693488fb2cfef564445528ec2a4f0b17c151cba
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-02-20 10:06:46 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = getString("Hvad hedder du?: ");
+        System.out.println("Hej " + name);
+    }
+
+    public static String getString(String s) {
+        System.out.print(s);
+        return scanner.nextLine();
     }
 }
