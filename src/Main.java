949f2e5991912442dca2268393b4c4ca324b828f
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-02-20 10:25:39 +0100

    created getInt() method

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,24 @@ public class Main {
     public static void main(String[] args) {
         String name = getString("Hvad hedder du?: ");
         System.out.println("Hej " + name);
+        int age = getInt("Hvor gammel er du?: ");
+        System.out.println("Du er " + age + " år gammel");
     }
 
     public static String getString(String s) {
         System.out.print(s);
         return scanner.nextLine();
+    }
+
+    public static int getInt(String s) {
+        int res;
+        while (true) {
+            try {
+                res = Integer.parseInt(getString(s));
+                return res;
+            } catch (NumberFormatException e) {
+                System.out.println("Du skal indtaste et tal");
+            }
+        }
     }
 }
